1c2256d5e02fc0c7385735b86cd135d5217bece5
Oliver Hine <git@oliy.co.uk>
2016-01-21 19:52:17 +0000

    Removing unused fn

--- src/pedestal/swagger/doc.clj
+++ src/pedestal/swagger/doc.clj
@@ -34,28 +34,6 @@
 (s/defschema Paths
   {s/Str {s/Keyword spec/Operation}})
 
-(defn deep-merge1
-  "Recursively merges maps.
-   If the first parameter is a keyword it tells the strategy to
-   use when merging non-map collections. Options are
-   - :replace, the default, the last value is used
-   - :into, if the value in every map is a collection they are concatenated
-     using into. Thus the type of (first) value is maintained."
-  {:arglists '([strategy & values] [values])}
-  [& values]
-  (let [[values strategy] (if (keyword? (first values))
-                            [(rest values) (first values)]
-                            [values :replace])]
-    (cond
-      (every? map? values)
-      (apply merge-with (partial deep-merge strategy) values)
-
-      (and (= strategy :into) (every? coll? values))
-      (reduce into values)
-
-      :else
-      (last values))))
-
 (s/defn gen-paths :- Paths
   "Generates swagger paths from an expanded route table.
   This function can also be used to generate documentation offline or for easy
